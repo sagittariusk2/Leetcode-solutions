7e59f7e2eb800da733a0b2680d4ad1365c7f2ff1
Ritesh Ranjan <73792817+sagittariusk2@users.noreply.github.com>
2022-01-15 12:38:16 +0530

    Time: 1358 ms (5.02%), Space: 232.2 MB (5.02%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        map<int, set<int>> mp;
+        for(int i=0; i<n; i++) {
+            mp[arr[i]].insert(i);
+        }
+        vector<int> dp(n, INT_MAX);
+        int x=1;
+        dp[0] = 0;
+        set<int> s1;
+        s1.insert(0);
+        while(!s1.empty()) {
+            set<int> s2;
+            for(auto i:s1) {
+                // cout << i << " ";
+                if(i+1<n and dp[i+1]==INT_MAX) {
+                    s2.insert(i+1);
+                    dp[i+1]=x;
+                }
+                if(i-1>=0 and dp[i-1]==INT_MAX) {
+                    s2.insert(i-1);
+                    dp[i-1]=x;
+                }
+                for(auto j:mp[arr[i]]) {
+                    s2.insert(j);
+                    dp[j] = min(x, dp[j]);
+                }
+                mp.erase(arr[i]);
+            }
+            // cout << endl;
+            x++;
+            s1 = s2;
+        }
+        
+        return dp[n-1];
+    }
+};
