1f3ac7a0be0491f8201a4d7fbfd76ee9e86b1d61
Ritesh Ranjan <73792817+sagittariusk2@users.noreply.github.com>
2022-01-16 12:47:50 +0530

    Time: 14 ms (52.40%), Space: 17 MB (63.67%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -2,21 +2,36 @@ class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
         int n=seats.size();
-        vector<int> l(n, INT_MAX), r(n, INT_MAX);
-        int x = -1;
+        // vector<int> l(n, INT_MAX), r(n, INT_MAX);
+        // int x = -1;
+        // for(int i=0; i<n; i++) {
+        //     if(seats[i]==1) x=i;
+        //     if(x!=-1) l[i] = i-x;
+        // }
+        // x=-1;
+        // for(int i=n-1; i>=0; i--) {
+        //     if(seats[i]==1) x=i;
+        //     if(x!=-1) r[i] = x-i;
+        // }
+        // int ans = 0;
+        // for(int i=0; i<n; i++) {
+        //     ans = max(ans, min(l[i], r[i]));
+        // }
+        
+        int max_z=0, f=0, ans=0, d=0;
         for(int i=0; i<n; i++) {
-            if(seats[i]==1) x=i;
-            if(x!=-1) l[i] = i-x;
+            if(seats[i]==0) f++;
+            else {
+                if(d==0) ans = max(ans, f);
+                d=1;
+                max_z = max(max_z, f);
+                ans = max(ans, (max_z+1)/2);
+                f=0;
+            }
         }
-        x=-1;
-        for(int i=n-1; i>=0; i--) {
-            if(seats[i]==1) x=i;
-            if(x!=-1) r[i] = x-i;
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            ans = max(ans, min(l[i], r[i]));
-        }
+        
+        ans = max(ans, f);
+        
         return ans;
     }
 };
