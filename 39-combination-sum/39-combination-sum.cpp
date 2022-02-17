ab11fbd69c36c7e4f7a3c83a3cb21f0f53847754
Ritesh Ranjan <73792817+sagittariusk2@users.noreply.github.com>
2022-02-17 22:09:21 +0530

    Time: 68 ms (28.03%), Space: 16.8 MB (34.95%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<vector<int>>> ans(target+1);
+        vector<int> v1;
+        ans[0].push_back(v1);
+        for(auto i:candidates) {
+            for(int j=0; j<=target; j++) {
+                if(j-i>=0) {
+                    for(auto k:ans[j-i]) {
+                        cout << i << " ";
+                        vector<int> x = k;
+                        x.push_back(i);
+                        ans[j].push_back(x);
+                    }
+                }
+            }
+        }
+        return ans[target];
+    }
+};
