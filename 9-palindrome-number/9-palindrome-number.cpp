cc4b19d7ab169dcdc45ca4782da47ee07c259d80
Ritesh Ranjan <73792817+sagittariusk2@users.noreply.github.com>
2022-03-25 15:08:01 +0530

    Time: 48 ms (5.38%), Space: 9.6 MB (6.43%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }    
+            
+        vector<int> v;
+        while(x!=0){
+            int r=x%10;
+            v.push_back(r);
+            x=x/10;
+        }
+        int len=v.size();
+        
+        for(int i=0;i<len;i++){
+           if(v[i]!=v[len-1-i]){
+               return false;
+           }
+        }
+        return true;
+    }
+};
