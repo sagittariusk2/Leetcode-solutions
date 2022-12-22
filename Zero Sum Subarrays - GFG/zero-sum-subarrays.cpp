0cc2e69a82cccf0f676148bc521e4a98007ee182
Ritesh Ranjan <73792817+sagittariusk2@users.noreply.github.com>
2022-12-22 22:58:59 +0530

    Added solution - LeetHub

---
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to count subarrays with sum equal to 0.
+    long long int findSubarray(vector<long long int> &arr, int n ) {
+        //code here
+        map<long long int, int> mpp;
+        long long int ans = 0, curr_sum=0;
+        mpp[0]=1;
+        for(auto i:arr) {
+            curr_sum += i;
+            ans += mpp[curr_sum];
+            mpp[curr_sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n; //input size of array
+       
+        vector<long long int> arr(n,0);
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i]; //input array elements
+        Solution ob;
+        cout << ob.findSubarray(arr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
